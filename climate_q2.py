f9d2c929cc178a0f76575ea315ea418ef0b87b0a
RFGriffith <reece.fagg@griffithuni.edu.au>
2023-09-15 17:30:43 +1000

    Modified climate_q2.py to import data from climate.csv, added output PNG to repository tracking

--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,24 @@ years = []
 co2 = []
 temp = []
 
+data=pd.read_csv("climate.csv")
+yearsCsv=data["Year"]
+co2Csv=data["CO2"]
+tempCsv=data["Temperature"]
+
+
+for x in yearsCsv:
+    print(x)
+    years.append(x)
+
+for y in co2Csv:
+    print(y)
+    co2.append(y)
+
+for z in tempCsv:
+    print(z)
+    temp.append(z)
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
