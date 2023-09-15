5f44cd8e5979682647b6c00263cbab7705fcf450
RFGriffith <reece.fagg@griffithuni.edu.au>
2023-09-15 17:11:18 +1000

    Updated climate_q1.py to import from climate.db, added output PNG to repository tracking

--- climate_q1.py
+++ climate_q1.py
@@ -1,9 +1,32 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
 
+connection=sqlite3.connect("./climate.db")
+yearsCursor=connection.cursor()
+co2Cursor=connection.cursor()
+tempCursor=connection.cursor()
+
+yearsCursor.execute("SELECT Year FROM ClimateData")
+result=yearsCursor.fetchall()
+for x in result:
+        years.append(x)
+co2Cursor.execute("SELECT CO2 FROM ClimateData")
+result=co2Cursor.fetchall()
+for y in result:
+        co2.append(y)
+tempCursor.execute("SELECT Temperature FROM ClimateData")
+result=tempCursor.fetchall()
+for z in result:
+        temp.append(z)
+connection.close()
+
+print(years)
+print(co2)
+print(temp)
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
